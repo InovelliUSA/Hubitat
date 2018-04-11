ede5f616254441d37f190e62f01a953c3c63389f
InovelliUSA <37669481+InovelliUSA@users.noreply.github.com>
2018-04-11 16:14:19 -0600

    namespace change

--- Drivers/inovelli-switch-nzw30.src/inovelli-switch-nzw30.groovy
+++ Drivers/inovelli-switch-nzw30.src/inovelli-switch-nzw30.groovy
@@ -18,7 +18,7 @@
  */
  
 metadata {
-	definition (name: "Inovelli Switch NZW30", namespace: "erocm123", author: "Eric Maycock") {
+	definition (name: "Inovelli Switch NZW30", namespace: "InovelliUSA", author: "Eric Maycock") {
 		capability "Switch"
 		capability "Refresh"
 		capability "Polling"
