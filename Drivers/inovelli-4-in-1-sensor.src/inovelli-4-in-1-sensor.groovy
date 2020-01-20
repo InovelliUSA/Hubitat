95904da33f07a7cd625832165db08fec32353de8
Sha'ul ben Yeshua <sgrayban@gmail.com>
2020-01-20 13:27:46 -0800

    Removed ST code and added powersource

--- Drivers/inovelli-4-in-1-sensor.src/inovelli-4-in-1-sensor.groovy
+++ Drivers/inovelli-4-in-1-sensor.src/inovelli-4-in-1-sensor.groovy
@@ -15,7 +15,7 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *  2020-01-20: scott_inovelli changes:
+ *  2020-01-20: Scott_Inovelli changes:
  *              added PowerSource
  *              added powersource preference
  *              removed ST code
