6c0717288d7e9ab615f503b173c12581c5e403b8
Inovelli <37669481+InovelliUSA@users.noreply.github.com>
2024-11-19 11:26:46 -0700

    add supportedFanSpeeds to configure method for Google Home support fix. Press configure button after updating the driver

--- Drivers/inovelli-fan-light-lzw36.src/inovelli-fan-light-lzw36.groovy
+++ Drivers/inovelli-fan-light-lzw36.src/inovelli-fan-light-lzw36.groovy
@@ -1,9 +1,9 @@
 /**
  *  Inovelli Fan + Light LZW36
  *  Author: Eric Maycock (erocm123)
- *  Date: 2021-11-30
+ *  Date: 2024-11-19
  *
- *  Copyright 2021 Inovelli / Eric Maycock
+ *  Copyright 2024 Inovelli / Eric Maycock
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -13,6 +13,8 @@
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
+ *  
+ *  2024-11-19: add supportedFanSpeeds to configure method for Google Home support fix. Press configure button after updating the driver.
  *  
  *  2021-11-30: Adding push & hold methods for Hubitat capability updates.
  *  
@@ -775,6 +777,7 @@ private zwaveValueToHuePercent(value){
 
 def configure() {
     if (infoEnable) log.info "${device.label?device.label:device.name}: configure()"
+    sendEvent(name: "supportedFanSpeeds", value: new groovy.json.JsonBuilder(["low","medium","high","on","off"]))
     def cmds = initialize()
     commands(cmds)
 }
